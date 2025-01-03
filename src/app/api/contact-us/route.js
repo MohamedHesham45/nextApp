d9a34927c5d2b8f4f59f0a8ee85c47ee40f835ee
MohamedHesham45 <mohmedadm733@gmail.com>
2025-01-03 20:48:36 +0200

    FEAT:handle contact-us

--- src/app/api/contact-us/route.js
+++ src/app/api/contact-us/route.js
@@ -1,32 +1,30 @@
-// import { NextResponse } from "next/server";
-// import clientPromise from "@/lib/mongodb";
-// import nodemailer from "nodemailer";
+import { NextResponse } from "next/server";
+import clientPromise from "@/lib/mongodb";
+import nodemailer from "nodemailer";
 
-// export async function POST(request) {
-//     const { name, email, message } = await request.json();
+export async function POST(request) {
+    const { name, email, message } = await request.json();
 
-//     // Create a Nodemailer transporter
-//     const transporter = nodemailer.createTransport({
-//         service: 'gmail', // or any other email service
-//         auth: {
-//             user: process.env.EMAIL_USER, // your email
-//             pass: process.env.EMAIL_PASS, // your email password
-//         },
-//     });
+    // Create a Nodemailer transporter
+    const transporter = nodemailer.createTransport({
+        service: 'gmail', 
+        auth: {
+            user: process.env.EMAIL_USER, 
+            pass: process.env.EMAIL_PASS, 
+        },
+    });
 
-//     // Set up email data
-//     const mailOptions = {
-//         from: email,
-//         to: process.env.EMAIL_USER, // your email
-//         subject: `Contact Us Form Submission from ${name}`,
-//         text: message,
-//     };
+    const mailOptions = {
+        to: email, 
+        subject: `Contact Us Form Submission from ${name}`,
+        text: message,
+    };
 
-//     // Send email
-//     try {
-//         await transporter.sendMail(mailOptions);
-//         return NextResponse.json({ message: 'Email sent successfully!' }, { status: 200 });
-//     } catch (error) {
-//         return NextResponse.json({ error: 'Failed to send email.' }, { status: 500 });
-//     }
-// }
+    // Send email
+    try {
+        await transporter.sendMail(mailOptions);
+        return NextResponse.json({ message: 'تم إرسال الرسالة بنجاح' }, { status: 200 });
+    } catch (error) {
+        return NextResponse.json({ message: 'حدث خطأ أثناء إرسال الرسالة' }, { status: 500 });
+    }
+}
