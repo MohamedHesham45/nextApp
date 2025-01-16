806b154bf029905acceb7ad764fcf27e6d9990d1
Mohamed Reda <muhammedreda6@gmail.com>
2025-01-16 03:02:24 +0200

    FEAT: Add pixel and conversion api events on buttons

--- src/app/product/[id]/page.js
+++ src/app/product/[id]/page.js
@@ -8,6 +8,7 @@ import { sanitizeHTML } from "@/components/ProductCard";
 import { useCartFavorite } from "@/app/context/cartFavoriteContext";
 import { toast } from "react-hot-toast";
 import ShoppingCartPage from "@/components/ShoppingCart";
+import useMetaConversion from "@/components/SendMetaConversion";
 
 export default function ProductPage() {
   return (
@@ -24,9 +25,10 @@ export function ProductDetails() {
   const [error, setError] = useState(null);
   const [selectedImageIndex, setSelectedImageIndex] = useState(0);
   const [isCartVisible, setIsCartVisible] = useState(false);
-
   const { cart, setCart, favorite, setFavorite } = useCartFavorite();
   const [cartQuantity, setCartQuantity] = useState(0);
+  const sendMetaConversion = useMetaConversion();
+
 
   const handleQuantityChange = (e, change) => {
     e.preventDefault();
@@ -73,6 +75,39 @@ export function ProductDetails() {
       toast.success("تم إزالة المنتج من المفضلة");
     } else {
       updatedFavorite = [...favorite, product];
+      var userAgent = navigator.userAgent;
+
+      fetch('https://api.ipify.org?format=json')
+        .then(response => response.json())
+        .then(async (data) => {
+          var ipAddress = data.ip;
+          fbq('track', 'AddToWishlist', {
+            product_name: product.title,
+            product_category: product.category,
+            product_ids: [product._id],
+            product_image: "https://sitaramall.com/" + product.images[0],
+            product_images: product.images.map(image => "https://sitaramall.com/" + image),
+            product_price: product.price,
+            product_price_after_discount: product.priceAfterDiscount || product.price,
+            product_quantity: product.quantity,
+            product_images: product.images,
+            value: product.priceAfterDiscount || product.price,
+            currency: 'EGP',
+            ip_address: ipAddress,
+            user_agent: userAgent
+          });
+          await sendMetaConversion('AddToWishlist', {
+            product_name: product.title,
+            product_category: product.category,
+            product_ids: [product._id],
+            product_image: "https://sitaramall.com/" + product.images[0],
+            product_images: product.images.map(image => "https://sitaramall.com/" + image),
+            product_price: product.price,
+            product_price_after_discount: product.priceAfterDiscount || product.price,
+            value: product.priceAfterDiscount || product.price,
+          }, ipAddress, userAgent);
+        })
+        .catch(error => console.error('Error fetching IP address:', error));
       toast.success("تم إضافة المنتج إلى المفضلة");
     }
 
@@ -92,7 +127,7 @@ export function ProductDetails() {
       (item) =>
         item._id === product._id &&
         JSON.stringify(item.selectedImages) ===
-          JSON.stringify(itemToAdd.selectedImages)
+        JSON.stringify(itemToAdd.selectedImages)
     );
 
     let updatedCart;
@@ -105,6 +140,40 @@ export function ProductDetails() {
     } else {
       updatedCart = [...cart, itemToAdd];
     }
+
+    var userAgent = navigator.userAgent;
+
+    fetch('https://api.ipify.org?format=json')
+      .then(response => response.json())
+      .then(async (data) => {
+        var ipAddress = data.ip;
+        fbq('track', 'AddToCart', {
+          product_name: product.title,
+          product_category: product.category,
+          product_ids: [product._id],
+          product_image: "https://sitaramall.com/" + product.images[0],
+          product_images: product.images.map(image => "https://sitaramall.com/" + image),
+          product_price: product.price,
+          product_price_after_discount: product.priceAfterDiscount || product.price,
+          product_quantity: product.quantity,
+          product_images: product.images,
+          value: product.priceAfterDiscount || product.price,
+          currency: 'EGP',
+          ip_address: ipAddress,
+          user_agent: userAgent
+        });
+        await sendMetaConversion('AddToCart', {
+          product_name: product.title,
+          product_category: product.category,
+          product_ids: [product._id],
+          product_image: "https://sitaramall.com/" + product.images[0],
+          product_images: product.images.map(image => "https://sitaramall.com/" + image),
+          product_price: product.price,
+          product_price_after_discount: product.priceAfterDiscount || product.price,
+          value: product.priceAfterDiscount || product.price,
+        }, ipAddress, userAgent);
+      })
+      .catch(error => console.error('Error fetching IP address:', error));
 
     setCart(updatedCart);
     localStorage.setItem("cart", JSON.stringify(updatedCart));
@@ -122,7 +191,7 @@ export function ProductDetails() {
       (item) =>
         item._id === product._id &&
         JSON.stringify(item.selectedImages) ===
-          JSON.stringify(itemToAdd.selectedImages)
+        JSON.stringify(itemToAdd.selectedImages)
     );
 
     let updatedCart;
@@ -131,9 +200,66 @@ export function ProductDetails() {
       setCart(updatedCart);
       localStorage.setItem("cart", JSON.stringify(updatedCart));
       toast.success("تم إضافة المنتج إلى السلة");
+      fetch('https://api.ipify.org?format=json')
+        .then(response => response.json())
+        .then(async (data) => {
+
+          var ipAddress = data.ip;
+          var userAgent = navigator.userAgent;
+
+          fbq('track', 'InitiateCheckout', {
+            product_name: product.title,
+            product_category: product.category,
+            product_ids: [product._id],
+            product_image: "https://sitaramall.com/" + product.images[0],
+            product_images: product.images.map(image => "https://sitaramall.com/" + image),
+            product_price: product.price,
+            product_price_after_discount: product.priceAfterDiscount || product.price,
+            product_quantity: product.quantity,
+            product_images: product.images,
+            value: product.priceAfterDiscount || product.price,
+            currency: 'EGP',
+            ip_address: ipAddress,
+            user_agent: userAgent
+          });
+          await sendMetaConversion('InitiateCheckout', {
+            product_name: product.title,
+            product_category: product.category,
+            product_ids: [product._id],
+            product_image: "https://sitaramall.com/" + product.images[0],
+            product_images: product.images.map(image => "https://sitaramall.com/" + image),
+            product_price: product.price,
+            product_price_after_discount: product.priceAfterDiscount || product.price,
+            value: product.priceAfterDiscount || product.price,
+          }, ipAddress, userAgent);
+        })
+        .catch(error => console.error('Error fetching IP address:', error));
       setIsCartVisible(true);
-    }else{
+    } else {
       setIsCartVisible(true);
+      // fetch('https://api.ipify.org?format=json')
+      // .then(response => response.json())
+      // .then(data => {
+
+      //   var ipAddress = data.ip;
+      //   var userAgent = navigator.userAgent;
+
+      //   fbq('track', 'InitiateCheckout', {
+      //     product_name: product.title,
+      //     product_category: product.category,
+      //     product_ids: [product._id],
+      //     product_image: "https://sitaramall.com/" + product.images[0],
+      //     product_price: product.price,
+      //     product_price_after_discount: product.priceAfterDiscount || product.price,
+      //     product_quantity: product.quantity,
+      //     product_images: product.images,
+      //     value: product.priceAfterDiscount || product.price,
+      //     currency: 'EGP',
+      //     ip_address: ipAddress,
+      //     user_agent: userAgent
+      //   });
+      // })
+      // .catch(error => console.error('Error fetching IP address:', error));
     }
 
   };
@@ -205,11 +331,10 @@ export function ProductDetails() {
                     <button
                       key={index}
                       onMouseEnter={() => setSelectedImageIndex(index)}
-                      className={`relative flex-shrink-0 w-16 h-16 sm:w-24 sm:h-24 rounded-lg overflow-hidden ${
-                        selectedImageIndex === index
-                          ? "ring-4 ring-amazon-blue"
-                          : "ring-2 ring-gray-200 hover:ring-blue-300"
-                      } transition-all duration-200`}
+                      className={`relative flex-shrink-0 w-16 h-16 sm:w-24 sm:h-24 rounded-lg overflow-hidden ${selectedImageIndex === index
+                        ? "ring-4 ring-amazon-blue"
+                        : "ring-2 ring-gray-200 hover:ring-blue-300"
+                        } transition-all duration-200`}
                     >
                       <img
                         src={image?.startsWith("/") ? image : `/${image}`}
@@ -284,75 +409,70 @@ export function ProductDetails() {
                 </span>
               </p>
 
-            <div className="space-y-4">
-              <div className="flex flex-wrap gap-4 sm:gap-6">
-                {cartQuantity > 0 ? (
-                  <div className="flex-1 flex items-center justify-center gap-2 sm:gap-4 bg-gray-100 p-2 rounded-full">
-                    <button
-                      onClick={(e) => handleQuantityChange(e, -1)}
-                      className="w-10 h-10 sm:w-12 sm:h-12 flex items-center justify-center bg-white text-gray-700 hover:bg-amazon-orange hover:text-white rounded-full transition-all duration-200 shadow-sm text-lg sm:text-xl font-semibold"
-                    >
-                      -
-                    </button>
-                    <span className="w-12 sm:w-16 text-center font-medium text-lg sm:text-xl">
-                      {cartQuantity}
-                    </span>
-                    <button
-                      onClick={(e) => handleQuantityChange(e, 1)}
-                      className={`w-10 h-10 sm:w-12 sm:h-12 flex items-center justify-center bg-white text-gray-700 rounded-full transition-all duration-200 shadow-sm text-lg sm:text-xl font-semibold ${
-                        cartQuantity >= product.quantity
+              <div className="space-y-4">
+                <div className="flex flex-wrap gap-4 sm:gap-6">
+                  {cartQuantity > 0 ? (
+                    <div className="flex-1 flex items-center justify-center gap-2 sm:gap-4 bg-gray-100 p-2 rounded-full">
+                      <button
+                        onClick={(e) => handleQuantityChange(e, -1)}
+                        className="w-10 h-10 sm:w-12 sm:h-12 flex items-center justify-center bg-white text-gray-700 hover:bg-amazon-orange hover:text-white rounded-full transition-all duration-200 shadow-sm text-lg sm:text-xl font-semibold"
+                      >
+                        -
+                      </button>
+                      <span className="w-12 sm:w-16 text-center font-medium text-lg sm:text-xl">
+                        {cartQuantity}
+                      </span>
+                      <button
+                        onClick={(e) => handleQuantityChange(e, 1)}
+                        className={`w-10 h-10 sm:w-12 sm:h-12 flex items-center justify-center bg-white text-gray-700 rounded-full transition-all duration-200 shadow-sm text-lg sm:text-xl font-semibold ${cartQuantity >= product.quantity
                           ? "opacity-50 cursor-not-allowed"
                           : "hover:bg-amazon-orange hover:text-white"
-                      }`}
-                      disabled={cartQuantity >= product.quantity}
-                    >
-                      +
-                    </button>
-                  </div>
-                ) : (
-                  <div className="flex-1 flex items-center justify-center gap-2 sm:gap-4 rounded-full">
-                    <button
-                      onClick={(e) => handleAddToCart(e, product)}
-                      className={`bg-amazon-orange hover:bg-amazon-orange-dark text-white rounded-full transition-all duration-200 hover:shadow-xl flex items-center justify-center text-base sm:text-lg font-semibold ${
-                        product.quantity === 0
-                        ? "opacity-50 cursor-not-allowed"
-                        : "hover:scale-105"
-                      } px-3 py-2 sm:px-4 sm:py-3 md:px-6 md:py-4 w-full `}
-                      disabled={product.quantity === 0}
+                          }`}
+                        disabled={cartQuantity >= product.quantity}
                       >
-                      <ShoppingCart className="h-5 w-5 sm:h-6 sm:w-6" />
-                      اضف الى العربة
-                    </button>
-                  </div>
-                )}
-                <button
-                  onClick={(e) => handleAddToFavorite(e, product)}
-                  className={`p-4 sm:p-5 bg-white hover:bg-red-50 text-gray-400 hover:text-red-500 rounded-full shadow-md transition-all duration-300 hover:scale-105 hover:shadow-xl border-2 border-gray-100 hover:border-red-500 ${
-                    favorite.some((item) => item._id === product._id)
-                    ? "text-red-500 border-red-500"
-                    : "text-gray-400 border-gray-100"
-                  }`}
-                  >
-                  <Heart
-                    className={`h-6 w-6 sm:h-7 sm:w-7 stroke-2 ${
-                      favorite.some((item) => item._id === product._id)
-                      ? "fill-red-500"
-                      : ""
-                    }`}
-                    />
-                </button>
-              </div>
+                        +
+                      </button>
                     </div>
+                  ) : (
+                    <div className="flex-1 flex items-center justify-center gap-2 sm:gap-4 rounded-full">
+                      <button
+                        onClick={(e) => handleAddToCart(e, product)}
+                        className={`bg-amazon-orange hover:bg-amazon-orange-dark text-white rounded-full transition-all duration-200 hover:shadow-xl flex items-center justify-center text-base sm:text-lg font-semibold ${product.quantity === 0
+                          ? "opacity-50 cursor-not-allowed"
+                          : "hover:scale-105"
+                          } px-3 py-2 sm:px-4 sm:py-3 md:px-6 md:py-4 w-full `}
+                        disabled={product.quantity === 0}
+                      >
+                        <ShoppingCart className="h-5 w-5 sm:h-6 sm:w-6" />
+                        اضف الى العربة
+                      </button>
+                    </div>
+                  )}
+                  <button
+                    onClick={(e) => handleAddToFavorite(e, product)}
+                    className={`p-4 sm:p-5 bg-white hover:bg-red-50 text-gray-400 hover:text-red-500 rounded-full shadow-md transition-all duration-300 hover:scale-105 hover:shadow-xl border-2 border-gray-100 hover:border-red-500 ${favorite.some((item) => item._id === product._id)
+                      ? "text-red-500 border-red-500"
+                      : "text-gray-400 border-gray-100"
+                      }`}
+                  >
+                    <Heart
+                      className={`h-6 w-6 sm:h-7 sm:w-7 stroke-2 ${favorite.some((item) => item._id === product._id)
+                        ? "fill-red-500"
+                        : ""
+                        }`}
+                    />
+                  </button>
+                </div>
+              </div>
 
               <button
-                className={`w-full bg-green-600 hover:bg-green-700 text-white py-3 sm:py-4 rounded-full transition-all duration-300 hover:shadow-xl text-base sm:text-lg font-semibold ${
-                  product.quantity === 0
-                    ? "opacity-50 cursor-not-allowed "
-                    : "hover:scale-105"
-                }`}
+                className={`w-full bg-green-600 hover:bg-green-700 text-white py-3 sm:py-4 rounded-full transition-all duration-300 hover:shadow-xl text-base sm:text-lg font-semibold ${product.quantity === 0
+                  ? "opacity-50 cursor-not-allowed "
+                  : "hover:scale-105"
+                  }`}
                 disabled={product.quantity === 0}
                 onClick={(e) => {
-                 handleBuyNow(e, product);
+                  handleBuyNow(e, product);
                 }}
               >
                 اطلب الآن
